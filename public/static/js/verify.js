9808d85f0851effb84255424e46585078acf1e97
unknown <>
2017-10-08 23:08:27 +0800

    手机号码验证

---
+++ public/static/js/verify.js
@@ -0,0 +1,10 @@
+function verify(){
+
+	var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
+	if(!myreg.test($("#phone").val())) 
+	{ 
+		alert('请输入有效的手机号码！'); 
+		return false; 
+	} 
+}
+
