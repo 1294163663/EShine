867d0bb9b7a928956da3f96e99f816b35b901523
unknown <>
2017-10-09 00:16:07 +0800

    second tryy

--- public/static/js/verify.js
+++ public/static/js/verify.js
@@ -1,10 +1,11 @@
 function verify(){
-
+    console.log("ok");
 	var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
 	if(!myreg.test($("#phone").val())) 
 	{ 
 		alert('请输入有效的手机号码！'); 
-		return false; 
+		return false; 	
 	} 
+	 
 }
 
