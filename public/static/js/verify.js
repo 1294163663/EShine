492c16b7ed484328a5173c05e276457966437b3d
unknown <>
2017-10-09 03:06:11 +0800

    111

--- public/static/js/verify.js
+++ public/static/js/verify.js
@@ -1,11 +1,21 @@
 function verify(){
     console.log("ok");
-	var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
-	if(!myreg.test($("#phone").val())) 
+	var myreg1 = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+	var myreg2 = /^[\u4e00-\u9fa5]{2,8}$/;
+	if (!myreg2.test($("#studentName").val())) {
+		alert('请输入正确的姓名！'); 
+		return false; 
+	}
+	if(!myreg1.test($("#phone").val())) 
 	{ 
 		alert('请输入有效的手机号码！'); 
 		return false; 	
 	} 
-	 
+	
+	else{
+		if(confirm("确定提交吗？")){
+			$("#myForm").submit();
+		}
+	}
 }
 
